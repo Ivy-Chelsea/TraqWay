5b30dcad1d4592f18bfc8ee5ce75b7605d9a7d3b
Ivy-Chelsea <ivychelsea20@gmail.com>
2024-08-31 13:59:21 +0300

    add home route

--- Traq/routes.py
+++ Traq/routes.py
@@ -5,3 +5,8 @@ from flask import render_template
 @app.route("/landing")
 def landing():
     return render_template('landing_page.html', title='landing page')
+
+
+@app.route("/home")
+def home():
+    return render_template('welcome.html', title='landing page')
